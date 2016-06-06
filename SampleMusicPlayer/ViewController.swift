6b4d2cfec2b784abdf219850c45071c1f66ab915
Than Dang <thandang@apples-mac-mini.local>
2016-06-06 15:17:00 +0700

    Initial Commit

---
+++ SampleMusicPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleMusicPlayer
+//
+//  Created by Than Dang on 6/6/16.
+//  Copyright © 2016 Than Dang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
